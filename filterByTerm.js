9f3c572e5a3de8742af265c02301b14a1c1cbd7f
Renato Lovizotto <lovizotto@gmail.com>
2019-09-13 09:47:31 -0300

    added a error

--- filterByTerm.js
+++ filterByTerm.js
@@ -1,6 +1,6 @@
 function filterByTerm(arr, searchTerm) {
     if (!searchTerm) throw new Error("Term is empty!");
-    if (!arr.length) throw ne Error("Array can't be empty");
+    if (!arr.length) throw new Error("Array can't be empty");
 
     return arr.filter(arrItem => arrItem.url.match(searchTerm));
 }
